98ced99936b2e18a64cdc2408fc6680f26eced19
ForCoding <forcoding255@gmail.com>
2024-11-15 19:23:24 +0530

    update1

--- moviefi/src/app/add/page.tsx
+++ moviefi/src/app/add/page.tsx
@@ -3,7 +3,7 @@ export default function AddMovie() {
     <div className="min-h-screen  flex flex-col items-center justify-center p-[24px] sm:p-[120px]">
       <div className="w-full "> Create a new movie </div>
       <div className="flex items-center justify-center">
-        <div className=""></div>
+        <div className="w-[473px] h-[504px] border border-red-50 flex items-center justify-center"></div>
         <div className="flex flex-col items-center justify-center "></div>
       </div>
     </div>
